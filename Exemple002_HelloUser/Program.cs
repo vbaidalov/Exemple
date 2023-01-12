c135308aea84ec086456805d1df04ef35d36ea7d
baidalov <vsb-posst@yande.ru>
2023-01-12 18:01:37 +0300

    Exemple002_HelloUser

--- Exemple002_HelloUser/Program.cs
+++ Exemple002_HelloUser/Program.cs
@@ -1,4 +1,4 @@
-﻿Console.WriteLine("Ввнедите ваше имя")
-sting username = Console.ReadLine();
-Console.WriteLine("Привет, ");
-Console.WriteLine(username);
+﻿Console.Write("Введите ваше имя ");
+string username = Console.ReadLine();
+Console.Write("Привет, ");
+Console.Write(username);
